ec67960ac68d13692464c49b04dcd7a948a11ded
parya <parya@DESKTOP-45A0TPE>
2025-08-07 19:26:18 -0400

    add ER and tolowercase to make case insensitive

--- app.js
+++ app.js
@@ -146,11 +146,11 @@ client.on('messageCreate', async message => {
     if (message.author.bot) return;
 
     //TODO add these words as database tables and add command to add and remove them at will
-    const forbiddenWords = ['league of legends', 'league', 'favonius', 'fav'];
+    const forbiddenWords = ['league of legends', 'league', 'favonius', 'fav', 'energy recharge', 'er'];
     const likedWords = ['coffie_wink', 'kafka', 'thighs', 'thigh', 'liyue', 'teriderp', 'yelan'];
     const messageContent = message.content.toLowerCase();
-    const matchedWordMinus = forbiddenWords.find(word => messageContent.search('\\b'+word+'\\b') !== -1);
-    const matchedWordPlus = likedWords.find(word => messageContent.search('\\b'+word+'\\b') !== -1);
+    const matchedWordMinus = forbiddenWords.find(word => messageContent.toLowerCase().search('\\b'+word+'\\b') !== -1);
+    const matchedWordPlus = likedWords.find(word => messageContent.toLowerCase().search('\\b'+word+'\\b') !== -1);
 
     if (matchedWordMinus) {
         const userId = message.author.id;
